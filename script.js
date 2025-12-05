89a9731b5ef02c4d077a264148f5915d230c66ed
Meet Jobanputra <meet.jobanputra024@gmail.com>
2025-12-05 07:29:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 var i = 0;
+var j = 0;
+
+let allTodos = {
+  todoData : [],
+};
+
 function onPress () {
-  let arr=[];
-  arr[i]=(document.createElement('div'));
-  arr[i].innerHTML=i;
-  document.body.appendChild(arr[i]);
+  var todoName = document.querySelector('#todo-name').value;
+  var todoDeadline = document.querySelector('#deadline').value;
+
+  let parent=[];
+  let TODO_Record_str = localStorage.getItem('userTODOs');
+  if (TODO_Record_str != null) {
+    
+  }
+  parent[i]= (document.createElement('div'));
+  parent[i].innerHTML = `<div>${todoName}</div><div>${todoDeadline}</div><button>remove</button>`
+  document.querySelector('#display-todos').appendChild(parent[i]);
+  parent[i].classList.add('display-todo-info');
+  document.querySelectorAll('.display-todo-info')[i].querySelectorAll('div')[0].classList.add('todo-name-div');
+
+  document.querySelectorAll('.display-todo-info')[i].querySelectorAll('div')[1].classList.add('todo-deadline-div');
+
+  document.querySelectorAll('.display-todo-info')[i].querySelectorAll('button')[0].classList.add('remove-todo-btn');
+
+  allTodos.todoData[i] = [todoName,todoDeadline];
   i++;
+
+  localStorage.setItem('userTODOs', JSON.stringify(allTodos));
 }
